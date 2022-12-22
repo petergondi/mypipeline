3e00e1fc85d96db1b61222b5b14be83077756171
peter <gondipeters@gmail.com>
2022-12-22 16:06:38 +0300

    First commit

---
+++ src/main/java/com/pipeline/pipeline/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.pipeline.pipeline.controller;
+
+import com.pipeline.pipeline.model.User;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+    @GetMapping
+    public List<User> findAllUsers() {
+        User myuser=new User();
+        myuser.setName("Mike");
+        myuser.setOccupation("Wing Man");
+        List<User> users=new ArrayList<>();
+        users.add(myuser);
+        return users;
+    }
+
+
+}
---
+++ src/main/java/com/pipeline/pipeline/model/User.java
@@ -0,0 +1,10 @@
+package com.pipeline.pipeline.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String occupation;
+}
+
