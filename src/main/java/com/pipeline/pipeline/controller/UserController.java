37594d53144ad2099fc85aec29d3b855f6e6fe4a
peter <gondipeters@gmail.com>
2023-01-09 19:50:50 +0300

    added maven files

--- src/main/java/com/pipeline/pipeline/controller/UserController.java
+++ src/main/java/com/pipeline/pipeline/controller/UserController.java
@@ -3,6 +3,7 @@ package com.pipeline.pipeline.controller;
 import com.pipeline.pipeline.model.User;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,15 @@ public class UserController {
         User myuser=new User();
         myuser.setName("Mike");
         myuser.setOccupation("Wing Man");
+        List<User> users=new ArrayList<>();
+        users.add(myuser);
+        return users;
+    }
+    @GetMapping
+    public List<User> findUser() {
+        User myuser=new User();
+        myuser.setName("Mike1");
+        myuser.setOccupation("Wing Man1");
         List<User> users=new ArrayList<>();
         users.add(myuser);
         return users;
